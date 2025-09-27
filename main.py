25d46ac1a9e70fc3e29595b3ec32da586f75b1a0
Sufyan Aslam <sufyanaslam1644@gmail.com>
2025-09-27 18:11:44 +0800

    Add HTML template for homepage

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-from fastapi import FastAPI
-app = FastAPI()
-@app.get("/")
-def home():
-    return {"message": "We are ready to build green tool"}
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+
+app = FastAPI(title="Green Toolkit")
+
+# Serve /static if you later add custom JS/CSS/images
+if os.path.isdir("static"):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/api/health")
+def health():
+    return {"ok": True}
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    # Data we can inject into the template
+    context = {
+        "request": request,
+        "title": "Green Toolkit",
+        "subtitle": "We are ready to build green tool",
+    }
+    return templates.TemplateResponse("index.html", context)
